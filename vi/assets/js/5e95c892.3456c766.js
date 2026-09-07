7354c6f3013524ce13b43baa8b5dde61d30bb837
Mac <mac@MacBook-Pro-cua-Mac.local>
2026-09-07 15:52:11 +0700

    multi language

---
+++ vi/assets/js/5e95c892.3456c766.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunktei_shield_docs||=[]).push([[647],{7121(e,s,r){r.r(s),r.d(s,{default:()=>l});r(6540);var a=r(4164),c=r(7559),t=r(5500),u=r(2831),d=r(1656),i=r(4848);function l(e){return(0,i.jsx)(t.e3,{className:(0,a.A)(c.G.wrapper.docsPages),children:(0,i.jsx)(d.A,{children:(0,u.v)(e.route.routes)})})}}}]);
